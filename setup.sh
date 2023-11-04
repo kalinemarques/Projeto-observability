1aea3722fe98822b5b648be92c1c1206c4f7f1ac
Matheus Santana <83383321+matheusfsantana@users.noreply.github.com>
2023-11-04 00:13:38 -0300

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+sudo sysctl -w vm.max_map_count=262144;
+
+# Navega para o diretório /exporters/http
+cd exporters/http/
+
+# Constrói a imagem Docker
+docker build -f Dockerfile . -t chamada-http:0.1
+
+# Navega para o diretório /exporters/apiastronauta
+cd ../apiastronauta/
+
+# Constrói a imagem Docker 
+docker build -f Dockerfile . -t api-astronauta:0.1
+
+# Volta para o diretorio
+cd ../../
+
+docker-compose up -d 
+
